7908c5b95a5a22215bcf4987129604c2d037d433
David Chou <davidchou2009@gmail.com>
2021-12-24 15:47:13 -0800

    add readme template

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ pwdGen.addEventListener("click",function(){
     //Prompt for Password length criteria
     var num = prompt("Please enter your desired Password length","Enter a number between 1 and 128");
     //Condition check 
-    while(num < 8 || num > 128 ){
+    while(num < 8 || num > 128 || num.includes(".")){
         num = prompt("Please enter your desired Password length","Please enter a valid number between 1 and 128");
     }
 
 
     //Ask for Desired Character types
-    // var items = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
-    // var itemsArray = items.split('')
+
     var lowerItems = "abcdefghijklmnopqrstuvwxyz";
     var lowerArray = lowerItems.split('')
     var upperItems = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
