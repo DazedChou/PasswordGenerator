7f4104b6fb45d01c10f0201c2c9b50465f511fb9
David Chou <davidchou2009@gmail.com>
2021-12-24 10:20:00 -0800

    validate input and at least one character type is selected

--- script.js
+++ script.js
@@ -10,15 +10,37 @@ pwdGen.addEventListener("click",function(){
     }
 
     //Ask for Desired Character types
-    var items = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
-    var itemsArray = items.split('')
+    // var items = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+    // var itemsArray = items.split('')
+    var lowerItems = "abcdefghijklmnopqrstuvwxyz";
+    var lowerArray = lowerItems.split('')
+    var upperItems = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var upperArray = upperItems.split('');
+    var numItems = "0123456789";
+    var numArray = numItems.split('');
+    var specialItems = "!@#$%^&*";
+    var specArray = specialItems.split('');
 
-    confirm("Do you want to include lowercase characters?");
-    confirm("Do you want to include uppercase characters?");
-    confirm("Do you want to include numeric characters?");
-    confirm("Do you want to include special characters?");
+    var lower = confirm("Do you want to include lowercase characters?");
+    var upper = confirm("Do you want to include uppercase characters?");
+    var numeral = confirm("Do you want to include numeric characters?");
+    var special = confirm("Do you want to include special characters?");
 
+    //Creating password and store into array
     //create empty password
-    var password =[];
+    var tempPassword =[];
 
+    //To make sure that atleast 1 char of desired choice is in password
+    if(lower){
+        tempPassword.push(lowerArray[Math.floor(Math.random()*26)]);
+    }
+    if(upper){
+        tempPassword.push(upperArray[Math.floor(Math.random()*26)]);
+    }
+    if(numeral){
+        tempPassword.push(numArray[Math.floor(Math.random()*10)]);
+    }
+    if(special){
+        tempPassword.push(specArray[Math.floor(Math.random()*8)]);
+    }
 })
