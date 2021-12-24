117116492e70e65342c648673528c05a40aa2e2d
David Chou <davidchou2009@gmail.com>
2021-12-24 09:00:44 -0800

    add password length criteria

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //Password Generator
+var pwdGen = document.querySelector("#button");
 
+pwdGen.addEventListener("click",function(){
+    //Prompt for Password length criteria
+    var num = prompt("Please enter your desired Password length","Enter a number between 1 and 128");
+    //Condition check 
+    while(num < 1 || num > 128){
+        num = prompt("Please enter your desired Password length","Please enter a valid number between 1 and 128");
+    }
+
+})
